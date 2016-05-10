a80536ff087eba3e994c5dd8cea8dba54878c822
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-05-10 14:00:53 +0300

    fix bag

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
             {
                 loader: "babel-loader",
                 query: {
-                    presets: ['es2015', 'stage-0']
+                    presets: ['babel-preset-es2015', 'babel-preset-stage-0']
                 }
             },
             {
