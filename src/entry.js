749d6fd866e3f98179eed7456474dcb8d07f467e
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-05-11 09:25:14 +0300

    add react

--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,9 @@
 import Todolist from './TodoList'
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render( <h1>Hello, world!</h1>, document.getElementById('example'));
 var list = new Todolist();
 var taskList = document.getElementById('list');
 
